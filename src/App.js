5df0a400f8aa7161bf3930f0de05a38c79cf4b2d
jeff-burns <1jeffreyburns@gmail.com>
2018-07-15 12:38:39 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">NewSource</a>
+          </nav>
+        </header>
+        <main className="container">
+          <h2>Show News Here</h2>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
