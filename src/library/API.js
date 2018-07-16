9d67a79906e432ecace0943b647c2085b19b4612
jeff-burns <1jeffreyburns@gmail.com>
2018-07-15 19:46:21 -0600

    fixed url

--- src/library/API.js
+++ src/library/API.js
@@ -1,8 +1,11 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api/v1/headlines' : 'https://googlenews-api.herokuapp.com//api/v1/dogs';
+const API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000/api/v1/headlines"
+    : "https://googlenews-api.herokuapp.com/api/v1/headlines";
 
 export default {
-    async getAll() {
-        const response = await fetch(API_URL);
-        return await response.json();
-    }
-}
+  async getAll() {
+    const response = await fetch(API_URL);
+    return await response.json();
+  }
+};
